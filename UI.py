9a1be70360e4fb68d93d11f595b7e580198ba666
Zolf-is-coding <fl-jm@mailo.com>
2023-05-31 10:18:38 +0200

    v0.5 ajout d une UI v2

--- UI.py
+++ UI.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import *
-from my_fonctions import are_my_paths_redondant
+from my_fonctions import  compare_extensions
+from my_fonctions import are_my_paths_redondant_v2
+import os
 
 
 def update_paths_visible_for_user ():
@@ -13,25 +15,56 @@ def update_paths_visible_for_user ():
 
     lbl_variable_content.config(text=Liste_chemins_lisibles, anchor='w')
 
-def browse_button():
-    # Allow user to select a directory and store it in global var
-    # called folder_path
-    filename = filedialog.askdirectory()
+
+def browse_button_v2():
     global globalpath
-    globalpath.append(filename)
+    globalpath.append(filedialog.askdirectory())
+    globalpath=are_my_paths_redondant_v2(globalpath)
     update_paths_visible_for_user()
     return
 
 
-def bouton_verification():
+def boutton_verifier_mes_doublons():
+    raw = 8
     global globalpath
-    So_many_errors=""
-    paths_To_check,So_many_errors = are_my_paths_redondant(globalpath)
-    lbl_error_content.config(text=So_many_errors, anchor='w')
-    globalpath = paths_To_check
-    lbl_variable_content.config(text=Liste_chemins_lisibles, anchor='w')
-    update_paths_visible_for_user()
-    return()
+   #print(globalpath)
+    # 3 lister les fichiers
+    #* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    special_i = 0
+    for path in globalpath :
+        print(path)
+        #print(f'path we are testing {path}')
+        #print(special_i)
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                #print(f'     file we are testing {os.path.join(file)}')
+                if compare_extensions(os.path.join(file), typefichier) == True  :
+                    all_files_to_compare[special_i] ={'path_w_file' : os.path.abspath(root),'path_wo_file' : os.path.dirname(file), 'filename':os.path.join(file),'extension':os.path.splitext(file)[-1].lower()}
+
+                    special_i=special_i+1
+                    #print(f"found files {os.path.join(root,file)}")
+
+    #pour verifier si j ai pete un truc decommenter en dessous :
+    #print(all_files_to_compare)
+    #===============================================================
+
+    List_of_my_doubles = []
+
+    # #    4 verifier les doublons de noms
+    # #toto = "Capture.png" in  all_files_to_compare.keys()
+    # print('jusque la tout va bien')
+    for id in all_files_to_compare :
+        File_in_work = str.lower(all_files_to_compare[id]['filename'])
+        #print(File_in_work)
+        for y in range(id+1, len(all_files_to_compare.keys())) :
+            #print(y)
+            #print(all_files_to_compare[y]['filename'])
+            if str.lower(all_files_to_compare[y]['filename'])==File_in_work :
+                print(f"its a match ! the file {File_in_work} in {all_files_to_compare[id]['path_w_file']}  is a double of {all_files_to_compare[y]['filename']} in {all_files_to_compare[y]['path_w_file']}" )
+                temp_Tupple = (all_files_to_compare[y]['path_w_file'],all_files_to_compare[id]['path_w_file'],all_files_to_compare[y]['filename'])
+                List_of_my_doubles.append(temp_Tupple)
+       # print(File_in_work)
+
 
 
 
@@ -45,9 +78,11 @@ fen1.geometry("800x800")
 globalpath = []
 
 Liste_chemins_lisibles = ''
+all_files_to_compare  = {}
 
-
-
+##assignet vairable for easier testing
+typefichier= ['.jpg',".png",".jpeg"]
+globalpath = ['C:/Users/altji/Pictures']
 
 ########### D E B U T    D U   C O D E ####
 tex1 = Label(fen1, text='Bonjour tout le monde !', fg='red')
@@ -57,7 +92,7 @@ tex1.grid(row=1, column=1, padx=5, pady=5)
 #ligne 2
 tex1 = Label(fen1, text='cliquer ici pour ajouter un chemin', fg='red')
 tex1.grid(row=2, column=1, padx=5, pady=5)
-bou1 = Button(fen1, text='ajouter', command = browse_button)
+bou1 = Button(fen1, text='ajouter', command = browse_button_v2)
 bou1.grid(row=2,column=2, padx=5, pady=5)
 
 
@@ -80,7 +115,7 @@ lbl_spacer.grid(row=5, column=5, sticky="e", padx=5, pady=5)
 
 
 #ligne 6
-bouton_check_my_paths = Button(fen1, text='Verifier mes chemins : ', command = bouton_verification)
+bouton_check_my_paths = Button(fen1, text='Verifier mes doublons : ', command = boutton_verifier_mes_doublons)
 bouton_check_my_paths.grid(row=6,column=1, padx=5, pady=5)
 
 
@@ -91,7 +126,25 @@ lbl_error_content.grid(row=6, column=2, sticky="e", padx=5, pady=5)
 lbl_spacer = tk.Label(fen1, text="")
 lbl_spacer.grid(row=7, column=5, sticky="e", padx=5, pady=5)
 
-button_check_my_doubles = Button(fen1, text='Verifier mes doublons  : ', command = bouton_verification)
-button_check_my_doubles.grid(row=6,column=1, padx=5, pady=5)
 
 fen1.mainloop()
+
+
+
+####  F O N C T I O N S ######
+
+
+
+
+
+def add_row_of_doublons(List_of_my_doubles,number) :
+
+
+    bouton_delete_left_file = Button(fen1, text='Verifier mes doublons  : ', command=print("tptp"))
+    bouton_delete_left_file.grid(row=number, column=1, padx=5, pady=5)
+    lbl_spacer = tk.Label(fen1, text="chemin_du_fichier_1")
+    lbl_spacer.grid(row=number, column=row, sticky="e", padx=5, pady=5)
+    lbl_spacer = tk.Label(fen1, text="chemin_du_fichier_1")
+    lbl_spacer.grid(row=number, column=row, sticky="e", padx=5, pady=5)
+    bouton_delete_right_file = Button(fen1, text='Verifier mes doublons  : ', command=print("tptp"))
+    bouton_delete_right_file.grid(row=number, column=1, padx=5, pady=5)
